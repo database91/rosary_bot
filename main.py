2e7599c6772d3a3d95ef1abd42e8b70bbc75109b
Кирилл <pythondatabase91@gmail.com>
2025-10-18 15:46:29 +0500

    какая то хрень

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import asyncio
 from aiogram import F
 from aiogram.filters.magic_data import MagicFilter
 from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.filters import CommandStart
 
 
 
 
-bot = Bot(token="8461458090:AAFeGNfdtGMRH0z18Efsg1EaO9wL_oGHZIA")
+bot = Bot(token="8461458090:AAFeGNfdtGMRH0z18Efsg1EaO9wL_oGHZIA",
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
 
 dp = Dispatcher()
 
@@ -17,18 +20,20 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start_func(message: types.Message):
     text = "<b>Привествую вас!</b> В нашем боте!  --&#9899--"
-    await message.answer(text=text,parse_mode=ParseMode.HTML)      
+    await message.answer(text=text)      
+
 
 @dp.message(F.photo)
 async def photo(message: types.Message):
     var_text_two = 'ЭТО ТЕКСТ ВТОРОЙ ПЕРЕМЕННОЙ'
     var_text = f"это просто прикольная переменная с f строкой {var_text_two}"
     await message.answer(text=var_text)
+  
 
 async def main():
     #пропускает апдейты когда выключен.
     await bot.delete_webhook(drop_pending_updates=True)
-    #добавляем в бота кнопку меню
+    
     #запускает бота в поллинг режиме
     await dp.start_polling(bot)
 
